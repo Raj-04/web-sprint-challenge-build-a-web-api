4d6a9dc5216e12a20f75af53dd7470960e69c17a
Raj-04 <1204patel.raj@gmail.com>
2021-12-03 15:08:04 -0500

    responds with updated action

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -31,6 +31,15 @@ router.post('/', checkCompleted, projectIdChecker, async (req, res, next) => {
   }
 })
 
+router.put('/:id', checkCompleted, checkActionId, async (req, res, next) => {
+  try {
+    const updatedAction = await Actions.update(req.params.id, req.body)
+    res.status(200).json(updatedAction)
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.use(handleError)
 
 module.exports = router
